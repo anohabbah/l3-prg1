e14b7d78c3345df8882e2df4ec68c50acdc61800
Abbah <anohabbah@gmail.com>
2017-12-10 14:48:11 +0100

    Code refactoring & bug fix

--- src/fr/istic/prg1/tp4/SmallSet.java
+++ src/fr/istic/prg1/tp4/SmallSet.java
@@ -67,7 +67,7 @@ public class SmallSet {
 	 * @pre 0 <= x <= 255
 	 */
 	public void add(int x) {
-		if ((x >= 0 && x < SmallSet.setSize) && !this.contains(x)) {
+		if (x >= 0 && x < SmallSet.setSize) {
 			this.tab[x] = true;
 		}
 	}
@@ -80,7 +80,7 @@ public class SmallSet {
 	 * @pre 0 <= x <= 255
 	 */
 	public void remove(int x) {
-        if ((x >= 0 && x < SmallSet.setSize) && this.contains(x)) {
+        if ((x >= 0 && x < SmallSet.setSize)) {
             this.tab[x] = false;
         }
 	}
@@ -191,11 +191,19 @@ public class SmallSet {
 	 * @return true, si this est inclus dans set2, false sinon
 	 */
 	public boolean isIncludedIn(SmallSet set2) {
-		// this est inclus dans set2 si chaque élémént de this appartient à set2
+		if (this == set2) {
+			return  true;
+		}
+
+		if (this.size() > set2.size()) {
+			return false;
+		}
+
+		// this est inclus dans set2 si chaque élémént de this appartient à set2.
         // Il suffit de trouver un élémént de this qui n'appartient pas à set2 pour
         // prouver que this n'est pas inclus dans set2.
 		for (int i = 0; i < SmallSet.setSize; i++) {
-			if (this.contains(i) && set2.contains(i)) {
+			if (this.contains(i) && !set2.contains(i)) {
 				return false;
 			}
 		}
@@ -220,15 +228,14 @@ public class SmallSet {
 		}
 
 		// this est égal à obj si les éléments de this et de obj sont identiques.
-		// Il suffit de trouver un élément de this (ou obj) qui n'appartient pas à obj (ou this)
+		int cpt = 0;
 		for (int i = 0; i < SmallSet.setSize; i++) {
-			if ((this.contains(i) && !((SmallSet) obj).contains(i)) || (!this.contains(i) && ((SmallSet) obj).contains(i))) {
-						return false;
+			if (this.tab[i] == ((SmallSet) obj).tab[i]) {
+				++cpt;
 			}
-
 		}
 
-		return true;
+		return cpt == SmallSet.setSize;
 	}
 
 	/**
